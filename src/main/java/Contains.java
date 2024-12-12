0243f003e02dae4249745d938cd74ea4d5708879
Brian Yang <yangbri1@msu.edu>
2024-12-12 08:55:38 +0000

    other ways using Collections doesn't seem to work, simpler way just using for-each loop alongside an if conditional gets the job done

--- src/main/java/Contains.java
+++ src/main/java/Contains.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 
 public class Contains {
 
@@ -11,6 +11,24 @@ public class Contains {
      * @return true if arr contains target, false otherwise.
      */
     public boolean arrayContains(int[] arr, int target){
+        // iterate through array of 'int' using for-each loop
+        for(int num : arr){
+            // if 'num' == 'target' in value
+            if(num == target){
+                // return true
+                return true;
+            }
+        }
+        // otw return false
         return false;
+
+        /* below two other ways not really working ... */
+        // convert array to List interface & use Collection's .contains() method to check if 'target' is present
+        // return Arrays.asList(arr).contains((Object)target);
+
+        // convert to Set as Set don't allow duplicates
+        // Set<Integer> set = new HashSet<>(Arrays.asList(arr));
+        // search for 'target', if found -> true
+        // return set.contains(target);
     }
 }
